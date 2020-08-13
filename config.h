8066a0660a88bae9b92e81cefc33bad6cfc510a4
yash3001 <yashchauhan197@gmail.com>
2020-08-13 19:09:54 +0530

    changed background colour for gruvbox theme

--- config.h
+++ config.h
@@ -118,7 +118,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
+	"#000000", /* 258 -> bg */  // Originally it was #282828 (default gruvbox background)
 	"#ebdbb2", /* 259 -> fg */
 };
 
